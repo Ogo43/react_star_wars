6395f6bf541c5cd8809b0ae73f868700cc789db1
Ogo43 <emehaogochukwu@gmail.com>
2023-05-23 17:34:27 +0100

    backup

---
+++ src/Component/StarWars.jsx
@@ -0,0 +1,124 @@
+
+
+function StarWars() {
+  return (
+    <div className='container'>
+      <header className='star_wars'>Star Wars</header>
+
+        <main className='content_general'>
+          <div className='sub-title1'>
+            <h3 className='title1'>A New Hope</h3>
+            <h5 className='date1'>May 27, 1977</h5>
+
+            <p className='content1'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+              Natus aliquid, maxime possimus exercitationem quos recusandae corrupti. 
+              Voluptate doloremque expedita repellat in dignissimos. 
+              Facere laborum quam expedita ex cumque aliquam voluptatem!
+            </p>
+
+            <footer>
+              <a href='https://finmavis.github.io/swapi-task/movie/1' className='link1'>
+                More Info
+              </a>
+            </footer>
+            
+          </div>
+        </main>
+          {/* <div className='sub-title2'>
+            <h3 className='title2'>A New Hope</h3>
+            <h5 className='date2'>May 27, 1977</h5>
+
+              <p className='content2'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Natus aliquid, maxime possimus exercitationem quos recusandae corrupti. 
+                Voluptate doloremque expedita repellat in dignissimos. 
+                Facere laborum quam expedita ex cumque aliquam voluptatem!
+              </p>
+
+              <div>
+                <a href='https://finmavis.github.io/swapi-task/movie/2' className='link2'>
+                  More Info
+                </a>
+              </div>
+          </div>
+
+          <div className='sub-title3'>
+            <h3 className='title3'>A New Hope</h3>
+            <h5 className='date3'>May 27, 1977</h5>
+
+              <p className='content3'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Natus aliquid, maxime possimus exercitationem quos recusandae corrupti. 
+                Voluptate doloremque expedita repellat in dignissimos. 
+                Facere laborum quam expedita ex cumque aliquam voluptatem!
+              </p>
+
+              <div>
+                <a href='https://finmavis.github.io/swapi-task/movie/3' className='link3'>
+                  More Info
+                </a>
+              </div>
+          </div>
+
+          <div className='sub-title4'>
+            <h3 className='title4'>A New Hope</h3>
+            <h5 className='date4'>May 27, 1977</h5>
+
+              <p className='content4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Natus aliquid, maxime possimus exercitationem quos recusandae corrupti. 
+                Voluptate doloremque expedita repellat in dignissimos. 
+                Facere laborum quam expedita ex cumque aliquam voluptatem!
+              </p>
+
+              <div>
+                <a href='https://finmavis.github.io/swapi-task/movie/4' className='link4'>
+                  More Info
+                </a>
+              </div>
+          </div>
+
+          <div className='sub-title5'>
+            <h3 className='title5'> A New Hope</h3>
+            <h5 className='date5'>May 27, 1977</h5>
+
+              <p className='content5'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Natus aliquid, maxime possimus exercitationem quos recusandae corrupti. 
+                Voluptate doloremque expedita repellat in dignissimos. 
+                Facere laborum quam expedita ex cumque aliquam voluptatem!
+              </p>
+
+              <div>
+                <a href='https://finmavis.github.io/swapi-task/movie/5' className='link5'>
+                  More Info
+                </a>
+              </div>
+          </div>
+
+          <div className='sub-title6'>
+            <h3 className='title6'>A New Hope</h3>
+            <h5 className='date6'>May 27, 1977</h5>
+
+              <p className='content6'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Natus aliquid, maxime possimus exercitationem quos recusandae corrupti. 
+                Voluptate doloremque expedita repellat in dignissimos. 
+                Facere laborum quam expedita ex cumque aliquam voluptatem!
+              </p>
+
+             <div>
+              <a href='https://finmavis.github.io/swapi-task/movie/6' className='link6'>
+                More Info
+              </a>
+             </div>
+          </div> */}
+
+        
+
+    </div>
+  )
+}
+
+export default StarWars;
